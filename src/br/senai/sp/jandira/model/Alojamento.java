4ad77ecfafacc7b22bd6f3deefcf7b68cd0d91ed
Gustavo <santosguhenrique@gmail.com>
2023-10-11 11:38:29 -0300

    lists

---
+++ src/br/senai/sp/jandira/model/Alojamento.java
@@ -0,0 +1,17 @@
+package br.senai.sp.jandira.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Alojamento {
+
+    List<Mamifero> alojamentoMamimefero = new ArrayList<>();
+
+    List<Felino> alojamentoFelino = new ArrayList<>();
+
+    List<Ave> alojamentoAve = new ArrayList<>();
+
+    public void adicionarAveAlojamento(Ave ave){
+        alojamentoAve.add(ave);
+    }
+}
